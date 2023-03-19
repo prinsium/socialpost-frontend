e3b26a7d48b3a5eef62a36a217e593246535d152
3vdi <unitcomics.world@proton.me>
2023-03-19 18:22:34 +0530

    login and signup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import './App.css';
+import "@mui/material";
+import "react-icons";
 
+import "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import ExploreIcon from '@mui/icons-material/Explore';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import ChatIcon from '@mui/icons-material/Chat';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import theme from "./theme";
+
+import LoginView from "./components/views/LoginView";
+import SignupView from "./components/views/SignupView";
+
+import { initiateSocketConnection, socket } from "./helpers/socketHelper";
+import { useEffect } from "react";
+import { BASE_URL } from "./config";
+import { io } from "socket.io-client";
 
 function App() {
-  const [value, setValue] = useState(0);
+  initiateSocketConnection();
+
   return (
-    <nav className="navbar fixed-bottom navbar-light">
-    <BottomNavigation
-  showLabels
-  value={value}
-  onChange={(event, newValue) => {
-    setValue(newValue);
-  }}
->
-  <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-  <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
-  <BottomNavigationAction label="" icon={<AddCircleOutlineIcon />} />
-  <BottomNavigationAction label="Chat" icon={<ChatIcon />} />
-  <BottomNavigationAction label="Account" icon={<AccountCircleIcon />} />
-</BottomNavigation>
-    </nav>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <Routes>
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/signup" element={<SignupView />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
