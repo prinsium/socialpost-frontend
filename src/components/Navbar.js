dd76fda9c196d63429c56f42d034ca6dd2394030
3vdi <unitcomics.world@proton.me>
2023-03-23 17:52:02 +0530

    search modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import {
   IconButton,
   Stack,
   TextField,
+  Typography,
+  Button,
+  Modal,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
@@ -21,6 +24,18 @@ import HorizontalStack from "./util/HorizontalStack";
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -31,6 +46,9 @@ const Navbar = () => {
   const [searchIcon, setSearchIcon] = useState(false);
   const [width, setWindowWidth] = useState(0);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     updateDimensions();
@@ -85,16 +103,27 @@ const Navbar = () => {
             <AiFillHome />
           </IconButton>
 
+        
         {!navbarWidth && (
-          <Box component="form" onSubmit={handleSubmit}>
-            <TextField
-              size="small"
-              label="Search for posts..."
-              sx={{ flexGrow: 1, maxWidth: 300 }}
-              onChange={handleChange}
-              value={search}
-            />
-          </Box>
+          <div>
+         <IconButton onClick={handleOpen}><AiOutlineSearch /></IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} component="form" onSubmit={handleSubmit}>
+             <TextField
+               size="small"
+              label="Search "
+               sx={{ flexGrow: 1, maxWidth: 300 }}
+               onChange={handleChange}
+               value={search}
+             />
+        </Box>
+      </Modal>
+      </div>
         )}
 
         <HorizontalStack>
